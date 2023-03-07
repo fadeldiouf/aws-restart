cab83f6d61907d41a5ec3d96ee624a0c4ab54e03
fadeldiouf <83551912+fadeldiouf@users.noreply.github.com>
2023-03-07 15:45:44 +0000

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,25 @@
+print("python as three numerique type: int, float and complex")
+myvalue=1
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type" + str(type(myvalue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) +" is of the data type" + str(type(myValue)))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) +" is of the data type" + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) +" is of the data type" + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) +" is of the data type" + str(type(myValue)))
